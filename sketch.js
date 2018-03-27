c3eda6bfe4d5c88809faa51f44da3a21c709df6b
Jason Barnes <jpbarne@g.clemson.edu>
2018-03-27 18:02:29 -0400

    Refactored and removed boundaries

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var numOfSeeds = 300;
+var numOfSeeds = 500;
 var noiseScale = 0.07;
 var zoneClicked = false;
 var s;
@@ -6,41 +6,36 @@ var clickX;
 var clickY;
 
 function setup() {
-  createCanvas(1600, 1600);
+  createCanvas(windowWidth, windowHeight);
   noStroke();
   noSmooth();
 
-  voronoiCellStrokeWeight(1);
-  voronoiCellStroke(0);
   voronoiSiteFlag(false);
-  //voronoiRndSites(numOfSeeds, 60);
+  voronoiCellStrokeWeight(0);
+  voronoiRndMinDist(1);
 
-  var ocean = color(0, 0, 200);
-  var grassland = color(0, 150, 0);
-  var lake = color(0, 255, 255);
-  var dirt = color(150, 100, 0);
   var snow = color(255);
 
   for (var y = 0; y < height/30; y++) {
     for (var x = 0; x < width/30; x++) {
       var noiseVal = noise(x*noiseScale, y*noiseScale);
-      ocean = color(noiseVal*100, noiseVal*200, 255);
-      grassland = color(noiseVal*100, 200-noiseVal*150, noiseVal*100);
-      dirt = color(150 - noiseVal*100, 100-noiseVal*70, 0);
-      if      (noiseVal < 0.40)  voronoiSite(x*100*noiseVal, y*100*noiseVal, ocean);
+      var ocean = color(noiseVal*100, noiseVal*200, 255);
+      var grassland = color(noiseVal*100, 200-noiseVal*150, noiseVal*100);
+      var dirt = color(150 - noiseVal*100, 100-noiseVal*70, 0);
+
+      if      (noiseVal < 0.42) voronoiSite(x*100*noiseVal, y*100*noiseVal, ocean);
       else if (noiseVal < 0.60) voronoiSite(x*100*noiseVal, y*100*noiseVal, grassland);
       else if (noiseVal < 0.67) voronoiSite(x*100*noiseVal, y*100*noiseVal, dirt);
       else if (noiseVal < 0.70) voronoiSite(x*100*noiseVal, y*100*noiseVal, grassland);
-      else if (noiseVal > 0.70) voronoiSite(x*100*noiseVal, y*100*noiseVal, color(255, 255, 255));
+      else 	   					voronoiSite(x*100*noiseVal, y*100*noiseVal, snow);
     }
   }
 
-  voronoi(width, height);
+  voronoi(windowWidth, windowHeight);
 }
 
 
 function draw() {
-  //background(255);
   voronoiDraw(0, 0);
   if(zoneClicked){
     text(s, 20, 60);
