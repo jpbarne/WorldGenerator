b85425f76c5d9919c7fd1623db8c16d97d4ce29b
jpbarne <jpbarne@g.clemson.edu>
2018-03-26 21:10:21 -0400

    Switched from hexagons to voronoi

--- sketch.js
+++ sketch.js
@@ -1,77 +1,35 @@
-//initial setup
+var numOfSeeds = 300;
+var noiseScale = 0.01;
+
 function setup() {
-  populate();
-  createCanvas(800, 800);
+  createCanvas(1600, 1600);
+  noStroke();
+  noSmooth();
+
+  voronoiCellStrokeWeight(1);
+  voronoiCellStroke(0);
+  voronoiSiteFlag(false);
+  voronoiRndSites(numOfSeeds, 60);
+
+  var ocean = color(0, 0, 200);
+  var grassland = color(0, 150, 0);
+  var lake = color(0, 255, 255);
+  var snow = color(255);
+
+  /*for (var y = 0; y < height/numOfSeeds; y++) {
+    for (var x = 0; x < width/numOfSeeds; x++) {
+      var noiseVal = noise(x*noiseScale, y*noiseScale);
+      if (noiseVal < 0.4) voronoiSite(x, y, color(0, 0, 200));
+      else if (noiseVal < 0.7) voronoiSite(x, y, color(0, 150, 0));
+      else if (noiseVal > 0.7) voronoiSite(x, y, color(255));
+    }
+  }*/
+
+  voronoi(width, height);
 }
 
-//continuous
+
 function draw() {
   background(255);
-
-  //draw cells
-  for (var i = 0; i < cells.length; i++) {
-    hexagon(cells[i]);
-  }
-}
-
-var cells = [];       //grid of hexagon cells
-var numCells = 10000; //TODO add ability to edit in browser
-
-//hexagon dimensions
-var hexW = 10;
-var hexH = Math.sqrt(3)/2 * hexW;
-
-//fill cells array
-function populate() {
-  var x = 0.5*hexW, y = 0.5*hexH, clock = 0;
-  noiseDetail(2, 0.6); 
-
-  for (var i = 0; i < numCells; i++) {
-    cells[i] = new Cell(x, y, noise(x, y)); //quasi-random elevation
-
-    //flat-side-up hexagon grid
-    x = (x+(0.75*hexW)) % (50*0.5*hexW);
-    if (x == 0.75*hexW) {
-      y += 0.2*hexH;
-      clock = 0;
-    }
-    else if (clock == 0) {
-      y += 0.2 * hexH;
-      clock = 1;
-    }
-    else if (clock == 1) {
-      y -= 0.2 * hexH;
-      clock = 0;
-    }
-  }
-}
-
-//cell class
-class Cell {
-  constructor(x, y, elev) {
-    this.x = x;
-    this.y = y;
-    this.elev = elev;
-  }
-}
-
-function hexagon(cell) {
-  //stroke(0);
-  strokeWeight(0);
-
-  if (cell.elev < 0.5) fill(0, 0, 255); // water
-  else if (cell.elev < 0.53) fill(230, 200, 0); // sand
-  else if (cell.elev < 0.75) fill(0, 255, 0); // grass
-  else if (cell.elev < 0.8) fill(144, 238, 144); // light grass
-  else fill(255); // snow
-
-  //draw hexagons
-  var angle = TWO_PI / 6;
-  beginShape();
-  for (var a = 0; a < TWO_PI; a+=angle) {
-    var sx = (cell.x + cos(a) * 2) * hexW/2;
-    var sy = (cell.y + sin(a) * 2) * hexW/2;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
+  voronoiDraw(0, 0, false, false);
 }
